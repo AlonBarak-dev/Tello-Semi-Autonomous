ac606bef095f6d69830ce110939404deba889bff
AlonBarak-dev <alonbarak3110@gmail.com>
2022-08-29 12:13:16 +0300

    Added ArUco code detection

--- tello_controller/XboxSubscriber.py
+++ tello_controller/XboxSubscriber.py
@@ -1,3 +1,4 @@
+from numpy import imag
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Joy
@@ -9,6 +10,8 @@ from djitellopy import tello
 from threading import Thread
 import cv2
 import argparse
+import imutils
+import sys
 
 class MinimalSubscriber(Node):
 
@@ -24,11 +27,36 @@ class MinimalSubscriber(Node):
         parser.add_argument('-debug', type=bool, help='Enable debug, lists messages in console', default=False)
         parser.add_argument('-video', type=str, help='Use as inputs a video file, no tello needed, debug must be True', default="")
         parser.add_argument('-vsize', type=list, help='Video size received from tello', default=(640,480))
+        parser.add_argument("-type", type=str,default="DICT_5X5_100", help="type of ArUCo tag to detect")
         parser.add_argument('-th', type=bool, help='Horizontal tracking', default=False)
         parser.add_argument('-tv', type=bool, help='Vertical tracking', default=True)
         parser.add_argument('-td', type=bool, help='Distance tracking', default=True)
         parser.add_argument('-tr', type=bool, help='Rotation tracking', default=True)
         self.args = parser.parse_args()
+
+        self.ARUCO_DICT = {
+            "DICT_4X4_50": cv2.aruco.DICT_4X4_50,
+            "DICT_4X4_100": cv2.aruco.DICT_4X4_100,
+            "DICT_4X4_250": cv2.aruco.DICT_4X4_250,
+            "DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
+            "DICT_5X5_50": cv2.aruco.DICT_5X5_50,
+            "DICT_5X5_100": cv2.aruco.DICT_5X5_100,
+            "DICT_5X5_250": cv2.aruco.DICT_5X5_250,
+            "DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
+            "DICT_6X6_50": cv2.aruco.DICT_6X6_50,
+            "DICT_6X6_100": cv2.aruco.DICT_6X6_100,
+            "DICT_6X6_250": cv2.aruco.DICT_6X6_250,
+            "DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
+            "DICT_7X7_50": cv2.aruco.DICT_7X7_50,
+            "DICT_7X7_100": cv2.aruco.DICT_7X7_100,
+            "DICT_7X7_250": cv2.aruco.DICT_7X7_250,
+            "DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
+            "DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
+            "DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
+            "DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
+            "DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
+            "DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
+        }
 
 
         self.subscription = self.create_subscription(
@@ -87,6 +115,57 @@ class MinimalSubscriber(Node):
         else:
             self.me.send_rc_control(int(a), int(b), int(c), int(d))
 
+    def detect_aruco(self, img):
+
+        image = imutils.resize(img, width=600)
+        # verify that the supplied ArUCo tag exists and is supported by
+        # OpenCV
+        if self.ARUCO_DICT[self.args.type] is None:
+            print("[INFO] ArUCo tag of '{}' is not supported".format(
+                self.args.type))
+            sys.exit(0)
+        # load the ArUCo dictionary, grab the ArUCo parameters, and detect
+        # the markers
+        for dict in self.ARUCO_DICT:
+            # arucoDict = cv2.aruco.Dictionary_get(self.ARUCO_DICT[self.args.type])
+            arucoDict = cv2.aruco.Dictionary_get(self.ARUCO_DICT[dict])
+            arucoParams = cv2.aruco.DetectorParameters_create()
+            (corners, ids, rejected) = cv2.aruco.detectMarkers(image, arucoDict, parameters=arucoParams)
+            # verify *at least* one ArUco marker was detected
+            if len(corners) > 0:
+                print("[INFO] detecting '{}' tags...".format(self.ARUCO_DICT[dict]))
+                # flatten the ArUco IDs list
+                ids = ids.flatten()
+                # loop over the detected ArUCo corners
+                for (markerCorner, markerID) in zip(corners, ids):
+                    # extract the marker corners (which are always returned in
+                    # top-left, top-right, bottom-right, and bottom-left order)
+                    corners = markerCorner.reshape((4, 2))
+                    (topLeft, topRight, bottomRight, bottomLeft) = corners
+                    # convert each of the (x, y)-coordinate pairs to integers
+                    topRight = (int(topRight[0]), int(topRight[1]))
+                    bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+                    bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+                    topLeft = (int(topLeft[0]), int(topLeft[1]))
+                    # draw the bounding box of the ArUCo detection
+                    cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
+                    cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
+                    cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
+                    cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
+                    # compute and draw the center (x, y)-coordinates of the ArUco
+                    # marker
+                    cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+                    cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+                    cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
+                    # draw the ArUco marker ID on the image
+                    cv2.putText(image, str(markerID),
+                        (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5, (0, 255, 0), 2)
+                    print("[INFO] ArUco marker ID: {}".format(markerID))
+            
+        return image
+
+
     def video(self):
         fobj = FollowObject(self.me, MODEL=self.args.model, PROTO=self.args.proto, CONFIDENCE=self.args.dconf, DEBUG=False, DETECT=self.args.obj)
         fobj.set_tracking( HORIZONTAL=self.args.th, VERTICAL=self.args.tv,DISTANCE=self.args.td, ROTATION=self.args.tr)
@@ -103,6 +182,7 @@ class MinimalSubscriber(Node):
             except Exception:
                 break
             
+            imghud = self.detect_aruco(imghud)
             fobj.draw_detections(imghud, ANONIMUS=False)
             cv2.imshow("TelloCamera",imghud)
             k = cv2.waitKey(1)
