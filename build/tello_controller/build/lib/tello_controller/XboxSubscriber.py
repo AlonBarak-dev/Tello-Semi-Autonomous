3e16eeaf4af4d6da5b8bd9984b370cfb76239c97
AlonBarak-dev <alonbarak3110@gmail.com>
2022-08-29 13:05:52 +0300

    added orientation for objects

--- build/tello_controller/build/lib/tello_controller/XboxSubscriber.py
+++ build/tello_controller/build/lib/tello_controller/XboxSubscriber.py
@@ -12,6 +12,8 @@ import cv2
 import argparse
 import imutils
 import sys
+from math import atan2, cos, sin, sqrt, pi
+import numpy as np
 
 class MinimalSubscriber(Node):
 
@@ -80,6 +82,64 @@ class MinimalSubscriber(Node):
         
         # self.me.takeoff()
         self.video_thread.start()
+    
+    def drawAxis(self, img, p_, q_, color, scale):
+        p = list(p_)
+        q = list(q_)
+        
+        ## [visualization1]
+        angle = atan2(p[1] - q[1], p[0] - q[0]) # angle in radians
+        hypotenuse = sqrt((p[1] - q[1]) * (p[1] - q[1]) + (p[0] - q[0]) * (p[0] - q[0]))
+        
+        # Here we lengthen the arrow by a factor of scale
+        q[0] = p[0] - scale * hypotenuse * cos(angle)
+        q[1] = p[1] - scale * hypotenuse * sin(angle)
+        cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv2.LINE_AA)
+        
+        # create the arrow hooks
+        p[0] = q[0] + 9 * cos(angle + pi / 4)
+        p[1] = q[1] + 9 * sin(angle + pi / 4)
+        cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv2.LINE_AA)
+        
+        p[0] = q[0] + 9 * cos(angle - pi / 4)
+        p[1] = q[1] + 9 * sin(angle - pi / 4)
+        cv2.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv2.LINE_AA)
+
+    def getOrientation(self, pts, img):
+        ## [pca]
+        # Construct a buffer used by the pca analysis
+        sz = len(pts)
+        data_pts = np.empty((sz, 2), dtype=np.float64)
+        for i in range(data_pts.shape[0]):
+            data_pts[i,0] = pts[i,0,0]
+            data_pts[i,1] = pts[i,0,1]
+        
+        # Perform PCA analysis
+        mean = np.empty((0))
+        mean, eigenvectors, eigenvalues = cv2.PCACompute2(data_pts, mean)
+        
+        # Store the center of the object
+        cntr = (int(mean[0,0]), int(mean[0,1]))
+        ## [pca]
+        
+        ## [visualization]
+        # Draw the principal components
+        cv2.circle(img, cntr, 3, (255, 0, 255), 2)
+        p1 = (cntr[0] + 0.02 * eigenvectors[0,0] * eigenvalues[0,0], cntr[1] + 0.02 * eigenvectors[0,1] * eigenvalues[0,0])
+        p2 = (cntr[0] - 0.02 * eigenvectors[1,0] * eigenvalues[1,0], cntr[1] - 0.02 * eigenvectors[1,1] * eigenvalues[1,0])
+        self.drawAxis(img, cntr, p1, (255, 255, 0), 1)
+        self.drawAxis(img, cntr, p2, (0, 0, 255), 5)
+        
+        angle = atan2(eigenvectors[0,1], eigenvectors[0,0]) # orientation in radians
+        ## [visualization]
+        
+        # Label with the rotation angle
+        label = "  Rotation Angle: " + str(-int(np.rad2deg(angle)) - 90) + " degrees"
+        textbox = cv2.rectangle(img, (cntr[0], cntr[1]-25), (cntr[0] + 250, cntr[1] + 10), (255,255,255), -1)
+        cv2.putText(img, label, (cntr[0], cntr[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        
+        return angle
+        
 
     def listener_callback(self, msg:Joy):
         """
@@ -174,7 +234,30 @@ class MinimalSubscriber(Node):
                         (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
                         0.5, (0, 255, 0), 2)
                     print("[INFO] ArUco marker ID: {}".format(markerID))
+        
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ 
+        # Convert image to binary
+        _, bw = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        
+        # Find all the contours in the thresholded image
+        contours, _ = cv2.findContours(bw, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+        
+        for i, c in enumerate(contours):
+        
+            # Calculate the area of each contour
+            area = cv2.contourArea(c)
             
+            # Ignore contours that are too small or too large
+            if area < 3700 or 100000 < area:
+                continue
+            
+            # Draw each contour only for visualisation purposes
+            cv2.drawContours(image, contours, i, (0, 0, 255), 2)
+            
+            # Find the orientation of each shape
+            self.getOrientation(c, image)
+        
         return image
 
 
